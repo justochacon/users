0f265176f7e240b547d1a0d734f4e8f758c5c802
ever <justochacon18@gmail.com>
2022-09-22 15:53:25 -0500

    inicial

---
+++ src/main/java/com/users/controller/UsersController.java
@@ -0,0 +1,28 @@
+package com.users.controller;
+
+import com.users.service.UsersService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/apiuser/v1")
+public class UsersController {
+    
+    @Autowired
+    UsersService usersService;
+    
+    @PostMapping("/user")
+    @ResponseStatus(HttpStatus.OK)
+    public Object user() {
+    	
+    	String 
+
+        return usersService.user();
+    }
+
+}
